2d3112f7aa75053de542c75e22c51b59f860f6c8
MarkSuttirak <93504873+suttirakcha@users.noreply.github.com>
2024-05-12 12:59:31 +0700

    update new words

--- src/components/TimesUp.tsx
+++ src/components/TimesUp.tsx
@@ -7,7 +7,7 @@ const TimesUp = () => {
 
   return (
     <div className="flex flex-col gap-y-8 items-center h-screen justify-center">
-      <h1 className="text-7xl text-red-500">Time's Up!</h1>
+      <h1 className="text-7xl text-red-500">Time's up!</h1>
 
       <div className="flex flex-col gap-y-4">
         <Button onClick={() => window.location.reload()} text="Try again"/>
